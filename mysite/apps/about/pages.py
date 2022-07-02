1f9d994653abead9bdebcedab06e8b983aefe6ca
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-07-02 19:55:40 +0200

    Add a tags

--- mysite/apps/about/pages.py
+++ mysite/apps/about/pages.py
@@ -1,11 +1,21 @@
-from aquilo import build, div, h1
+from aquilo import build, div, h1, a
 
 
 def page_about():
-    root = div(h1("Det här är min Om-sektion"))
+    root = div(
+        [
+            h1("Det här är min Om-sektion"), 
+            a("Go to home", href="/")
+        ]
+    )
     return build(root, title="Om")
 
 
 def page_projects():
-    root = div(h1("Det här är min Projekt-sektion"))
+    root = div(
+        [
+            h1("Det här är min Projekt-sektion"),
+            a("Go to home", href="/")
+        ]
+    )
     return build(root, title="Projekt")
--- mysite/apps/home/pages.py
+++ mysite/apps/home/pages.py
@@ -1,6 +1,12 @@
-from aquilo import build, div, h1
+from aquilo import build, div, h1, a
 
 
 def page_home():
-    root = div(h1("Hej, jag heter Vuyo"))
+    root = div(
+        [
+            h1("Hej, jag heter Vuyo"),
+            a("Go to about", href="/about/"),
+            a("Go to projects", href="/about/projects/"),
+        ]
+    )
     return build(root, title="Velkommen til min hemsida")
